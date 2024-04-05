8b979f3a162e37b325bd5fc2b46543aaad4df12d
PuzzlingGGG <138084889+PuzzlingGGG@users.noreply.github.com>
2024-04-05 15:38:43 -0400

    Update studioview.jsx

--- src/components/tw-studioview/studioview.jsx
+++ src/components/tw-studioview/studioview.jsx
@@ -10,17 +10,17 @@ import styles from './studioview.css';
 const messages = defineMessages({
     authorAttribution: {
         defaultMessage: 'by {author}',
-        description: 'Displayed in StudioView under project title to credit creator',
+        description: 'Displayed under commit message to credit author',
         id: 'tw.studioview.authorAttribution'
     },
     hoverText: {
-        defaultMessage: '{title} by {author}',
-        description: 'Displayed in StudioView when hovering on a project',
+        defaultMessage: 'Commit {title} by {author}',
+        description: 'Displayed when hovering over a commit',
         id: 'tw.studioview.hoverText'
     },
     error: {
-        defaultMessage: 'There was an error loading the next page of projects.',
-        description: 'Displayed in StudioView when an error occurs',
+        defaultMessage: 'There was an error loading the commits.',
+        description: 'Displayed when an error occurs',
         id: 'tw.studioview.error'
     }
 });
@@ -36,27 +36,16 @@ class StudioViewComponent extends React.Component {
     componentDidMount () {
         this.studioView = new StudioView(this.props.id);
         this.studioView.messages.AUTHOR_ATTRIBUTION = this.props.intl.formatMessage(messages.authorAttribution, {
-            // studioview uses $-based variables
             author: '$author'
         });
         this.studioView.messages.PROJECT_HOVER_TEXT = this.props.intl.formatMessage(messages.hoverText, {
-            // studioview uses $-based variables
             author: '$author',
             title: '$title'
         });
         this.studioView.messages.LOAD_ERROR = this.props.intl.formatMessage(messages.error);
-        if (this.props.placeholder) {
-            this.studioView.addPlaceholders();
-        } else {
-            this.studioView.loadNextPage();
-        }
+        this.studioView.loadNextPage();
         this.studioView.onselect = this.handleSelect;
         this.el.appendChild(this.studioView.root);
-    }
-    componentDidUpdate (prevProps) {
-        if (prevProps.placeholder && !this.props.placeholder) {
-            this.studioView.loadNextPage();
-        }
     }
     handleSelect (id) {
         this.props.onSelect(id);
@@ -79,7 +68,6 @@ class StudioViewComponent extends React.Component {
 StudioViewComponent.propTypes = {
     id: PropTypes.string.isRequired,
     intl: intlShape.isRequired,
-    placeholder: PropTypes.bool,
     onSelect: PropTypes.func.isRequired
 };
 
